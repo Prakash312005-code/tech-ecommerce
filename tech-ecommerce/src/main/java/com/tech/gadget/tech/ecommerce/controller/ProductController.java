4514890bcb8ae33f2fbae52ac8852dbccd13a8ea
Prakash A <prakash.a312005@gmail.com>
2026-09-08 22:41:45 +0530

    Refactor ProductController for cleaner code

--- tech-ecommerce/src/main/java/com/tech/gadget/tech/ecommerce/controller/ProductController.java
+++ tech-ecommerce/src/main/java/com/tech/gadget/tech/ecommerce/controller/ProductController.java
@@ -1,7 +1,8 @@
 package com.tech.gadget.tech.ecommerce.controller;
 
 import java.util.List;
-
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
