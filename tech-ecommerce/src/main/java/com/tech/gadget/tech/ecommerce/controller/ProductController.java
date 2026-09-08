4b92ced77e6b6b38d5e7776d7b3e90c88018c391
Prakash A <prakash.a312005@gmail.com>
2026-09-08 13:33:22 +0530

    Add product POST API

--- tech-ecommerce/src/main/java/com/tech/gadget/tech/ecommerce/controller/ProductController.java
+++ tech-ecommerce/src/main/java/com/tech/gadget/tech/ecommerce/controller/ProductController.java
@@ -1,38 +1,47 @@
 package com.tech.gadget.tech.ecommerce.controller;
-	import java.util.List;
+
+import java.util.List;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tech.gadget.tech.ecommerce.entity.Product;
 import com.tech.gadget.tech.ecommerce.service.ProductService;
 
-
-	@RestController
-	@RequestMapping("/api/products")
-	@CrossOrigin(origins = "http://localhost:3000")
-	
+@RestController
+@RequestMapping("/api/products")
+@CrossOrigin(origins = {
+        "http://localhost:3000",
+        "https://tech-gadget-store-two.vercel.app"
+})
 public class ProductController {
-	    private final ProductService productService;
 
-	    public ProductController(ProductService productService) {
-	        this.productService = productService;
-	    }
+    private final ProductService productService;
 
-	    // Get all products
-	    @GetMapping
-	    public List<Product> getAllProducts() {
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
 
-	        return productService.getAllProducts();
-	    }
+    // GET ALL PRODUCTS
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
 
-	    // Get product by ID
-	    @GetMapping("/{id}")
-	    public Product getProductById(@PathVariable Long id) {
+    // GET PRODUCT BY ID
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable Long id) {
+        return productService.getProductById(id);
+    }
 
-	        return productService.getProductById(id);
-	    }
-	}
-
+    // ADD PRODUCT
+    @PostMapping
+    public Product addProduct(@RequestBody Product product) {
+        return productService.addProduct(product);
+    }
+}
